edf1769657a407586d3025a4e0cbf309807ad1ee
sandeep <sandeep01.gautam@gmail.com>
2021-10-03 18:26:43 +1100

    New app being built

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import ImageBackground from "react-native/Libraries/Image/ImageBackground";
+import StyledButton from "../StyleButton";
 import styles from "./styles";
 
 const CarItem = () => {
@@ -14,6 +15,20 @@ const CarItem = () => {
         <Text style={styles.title}>Tesla</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.log("Customer Order was pressed");
+        }}
+      />
+      <StyledButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.log("Customer Order was pressed");
+        }}
+      />
     </View>
   );
 };
